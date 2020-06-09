d90682026e1bc0eb9ccecdf905a055d47526c129
Đinh Ngọc Mai <maidn@rabiloo.com.vn>
2020-06-09 15:53:53 +0700

    some examples with java

--- ArgumentExample.java
+++
@@ -1,8 +0,0 @@
-public class ArgumentExample {
-    public static void main(String[] args) {
-//        System.out.println("hello world");
-        for(int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-    }
-}
